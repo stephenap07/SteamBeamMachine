75801845c10319a360f52f66c140c1b00677099e
Stephen Pridham <stephenap07@gmail.com>
2014-11-21 21:28:45 -0500

    Add tile map and image

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,21 @@
 #include <SFML/Graphics.hpp>
 
+struct Node {
+   sf::Sprite sprite;
+};
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(640, 640), "SFML works!");
+    sf::CircleShape shape(100.0f);
     shape.setFillColor(sf::Color::Green);
+
+    sf::Texture tex;
+    tex.loadFromFile("smw_ground.png");
+
+    Node n;
+    n.sprite.setTexture(tex);
+    n.sprite.setTextureRect(sf::Rect<int>(569, 99, 16, 16));
 
     while (window.isOpen())
     {
@@ -16,7 +27,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(n.sprite);
         window.display();
     }
 
