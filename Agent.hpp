5a5eeae47f6a89196d2f4ebd290dff39f963543d
Stephen Pridham <stephenap07@gmail.com>
2014-11-26 15:00:31 -0500

    Add event handling & playback of events

--- Agent.hpp
+++ Agent.hpp
@@ -5,6 +5,18 @@
 #include <SFML/Graphics.hpp>
 #include "AnimatedSprite.hpp"
 
+
+struct PhysicalProperties {
+    bool isOnGround;
+    float terminalSpeed;
+    float terminalVelocity;
+    float jumpAcceleration;
+    float landAcceleration;
+    float skyAcceleration;
+    sf::Vector2f currentSpeed;
+    sf::Vector2f targetSpeed;
+};
+
 struct Controller;
 
 struct Agent {
@@ -21,6 +33,7 @@ struct Agent {
     direction currentDir;
 	std::unique_ptr<AnimatedSprite> animatedSprite;
     std::vector<std::shared_ptr<Controller>> controllers;
+    PhysicalProperties phys;
 };
 
 #endif // AGENT_H_
