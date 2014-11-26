9ad903baf9e18300b577760cd6125a75c2b412e7
Stephen Pridham <stephenap07@gmail.com>
2014-11-26 11:55:08 -0500

    Add new controller functionality, add basic physics

--- Agent.hpp
+++ Agent.hpp
@@ -5,116 +5,22 @@
 #include <SFML/Graphics.hpp>
 #include "AnimatedSprite.hpp"
 
+struct Controller;
 
 struct Agent {
-	enum anim_type {
-		WALK_RIGHT = 0,
-		WALK_LEFT,
-		SLIDE_RIGHT,
-		SLIDE_LEFT
-	};
+    enum direction {
+        LEFT=0,
+        RIGHT,
+        DOWN,
+        UP
+    };
 
-	Agent()
-	{
-		texture.loadFromFile("agent.png");
+    virtual void update(sf::Time deltaTime) {};
 
-		Animation &walkingAnimationRight = animations[anim_type::WALK_RIGHT];
-		walkingAnimationRight.setSpriteSheet(texture);
-		walkingAnimationRight.addFrame(sf::IntRect(0, 0, 15, 20));
-		walkingAnimationRight.addFrame(sf::IntRect(15, 0, 15, 20));
-		walkingAnimationRight.addFrame(sf::IntRect(30, 0, 15, 20));
-		walkingAnimationRight.addFrame(sf::IntRect(0, 20, 15, 20));
-
-		Animation &walkingAnimationLeft = animations[anim_type::WALK_LEFT];
-		walkingAnimationLeft.setSpriteSheet(texture);
-		walkingAnimationLeft.addFrame(sf::IntRect(15, 20, 15, 20));
-		walkingAnimationLeft.addFrame(sf::IntRect(30, 20, 15, 20));
-		walkingAnimationLeft.addFrame(sf::IntRect(45, 20, 15, 20));
-		walkingAnimationLeft.addFrame(sf::IntRect(0, 40, 15, 20));
-
-		Animation &slideAnimationLeft = animations[anim_type::SLIDE_LEFT];
-		slideAnimationLeft.setSpriteSheet(texture);
-		slideAnimationLeft.addFrame(sf::IntRect(15, 40, 15, 20));
-
-		Animation slideAnimationRight = animations[anim_type::SLIDE_RIGHT];
-		slideAnimationRight.setSpriteSheet(texture);
-		slideAnimationRight.addFrame(sf::IntRect(30, 40, 15, 20));
-
-		animIndex = anim_type::WALK_RIGHT;
-		faceRight = true;
-
-		// Mario - 15 frames per 0.5s
-		// Animation - 4 frames, 0.5s / (15/4) = 0.13
-		animatedSprite = std::unique_ptr<AnimatedSprite>(new AnimatedSprite(sf::seconds(0.13f), true, false));
-		animatedSprite->setPosition(sf::Vector2f(0, 0));
-		speed = 80.0f;
-	}
-
-	void update(sf::Time timeDelta)
-	{
-        animatedSprite->play(animations[animIndex]);
-        animatedSprite->update(timeDelta);
-	}
-
-	bool faceRight;
-	float speed;
 	int animIndex;
-
+    direction currentDir;
 	std::unique_ptr<AnimatedSprite> animatedSprite;
-
-	Animation animations[4];
-	sf::Texture texture;
-};
-
-
-struct KeyboardController {
-	KeyboardController()
-	{
-		noKeyWasPressed = true;
-	}
-
-	void update(sf::Time timeDelta)
-	{
-        sf::Vector2f movement(0.f, 0.f);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            //if (vec.x > 0.0f) {
-                //currentAnimation = &slideAnimationRight;
-            //} else {
-            agent->animIndex = Agent::anim_type::WALK_LEFT;
-            //}
-
-            movement.x -= agent->speed;
-            agent->faceRight = false;
-            noKeyWasPressed = false;
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            //if (vec.x < 0.0f) {
-             //   currentAnimation = &slideAnimationLeft;
-            //} else {
-            agent->animIndex = Agent::anim_type::WALK_RIGHT;
-            //}
-
-            movement.x += agent->speed;
-            agent->faceRight = true;
-            noKeyWasPressed = false;
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-            // jump
-        }
-
-        agent->animatedSprite->move(movement * timeDelta.asSeconds());
-
-        if (noKeyWasPressed) {
-            agent->animatedSprite->stop();
-        }
-        noKeyWasPressed = true;
-	}
-
-	bool noKeyWasPressed;
-	Agent *agent;
+    std::vector<std::shared_ptr<Controller>> controllers;
 };
 
 #endif // AGENT_H_
---
+++ Steamy.hpp
@@ -0,0 +1,21 @@
+#ifndef STEAMY_H_
+#define STEAMY_H_
+
+#include "Agent.hpp"
+
+struct Steamy : public Agent {
+	Steamy();
+	virtual void update(sf::Time timeDelta);
+
+	enum anim_type {
+		WALK_RIGHT = 0,
+		WALK_LEFT,
+		SLIDE_RIGHT,
+		SLIDE_LEFT
+	};
+
+	Animation animations[4];
+	sf::Texture texture;
+};
+
+#endif
