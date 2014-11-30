5fdf6185da3a1a3450a6f6d796e46f197ce8d3b3
Stephen Pridham <stephenap07@gmail.com>
2014-11-29 23:32:25 -0500

    Fix int ptr cast from void ptr

--- CollisionController.hpp
+++ CollisionController.hpp
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include <vector>
+#include <cstdint>
+#include <inttypes.h>
 #include <Box2D/Box2D.h>
 #include "Controller.hpp"
 
@@ -14,23 +16,23 @@ struct CollisionManager : public Controller {
 
         void BeginContact(b2Contact* contact) {
             //check if fixture A was the foot sensor
-            void* fixtureUserData = contact->GetFixtureA()->GetUserData();
-            if ( (int)fixtureUserData == 3 )
+            int *fixtureUserData = reinterpret_cast<int*>(contact->GetFixtureA()->GetUserData());
+            if ( fixtureUserData == (int*)3 )
                 numFootContacts++;
             //check if fixture B was the foot sensor
-            fixtureUserData = contact->GetFixtureB()->GetUserData();
-            if ( (int)fixtureUserData == 3 )
+            fixtureUserData = reinterpret_cast<int*>(contact->GetFixtureB()->GetUserData());
+            if ( fixtureUserData == (int*)3 )
                 numFootContacts++;
         }
 
         void EndContact(b2Contact* contact) {
             //check if fixture A was the foot sensor
-            void* fixtureUserData = contact->GetFixtureA()->GetUserData();
-            if ( (int)fixtureUserData == 3 )
+            int *fixtureUserData = reinterpret_cast<int*>(contact->GetFixtureA()->GetUserData());
+            if ( fixtureUserData == (int*)3 )
                 numFootContacts--;
             //check if fixture B was the foot sensor
-            fixtureUserData = contact->GetFixtureB()->GetUserData();
-            if ( (int)fixtureUserData == 3 )
+            fixtureUserData = reinterpret_cast<int*>(contact->GetFixtureB()->GetUserData());
+            if ( fixtureUserData == (int*)3 )
                 numFootContacts--;
         }
 
