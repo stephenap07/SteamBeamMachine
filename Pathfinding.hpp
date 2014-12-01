25b906c66680f7cd1f8eadf076eaa05521ed960d
Stephen Pridham <stephenap07@gmail.com>
2014-12-01 04:23:43 -0500

    Checkpoint

--- Pathfinding.hpp
+++ Pathfinding.hpp
@@ -18,33 +18,48 @@ struct Node {
     const bool operator==(const Node &other) { return (x == other.x && y == other.y); }
     const bool operator>(const Node &other) { return f() > other.f(); }
     const bool operator<(const Node &other) { return f() < other.f(); }
+
+    int distanceTo(const Node &other) const
+    {
+        return std::sqrt((other.x - x) * (other.x - x) + (other.y - y) * (other.y - y));   
+    }
 };
 
 
 class Pathfinder {
 
-typedef std::list<Node*>::iterator NodePtrIter;
+    typedef std::list<Node*>::iterator NodeIter;
+
+    struct Point {
+        int x, y;
+    };
 
 public:
-    Pathfinder() :m_tileMap(nullptr) {}
-    Pathfinder(TileMap *tileMap) :m_tileMap(tileMap) {}
+    Pathfinder() :m_tileMap(nullptr) {initSides();}
+    Pathfinder(TileMap *tileMap) :m_tileMap(tileMap) {initSides();}
 
     std::vector<Node> getPath(Node start, Node goal);
     void setMap(TileMap *tileMap);
 
 private:
-    int distance(Node start, Node goal)
+    int manhattanDistance(Node start, Node goal)
     {
         return std::abs(goal.x - start.x) + std::abs(goal.y - start.y);
     }
 
-    std::vector<Node> identifySuccessors(Node current, Node start, Node goal);
-    Node jump(int x, int y, int dX, int dY, const Node &start, const Node &goal);
-    Node *nextNode(std::list<Node*> &nodeList);
-    std::vector<Node*> getNeighbors(const Node *node);
+    void setNodes(Node goal);
+
+    std::vector<Node*> identifySuccessors(const Node *current, Node *start, Node *goal);
+    std::vector<Node*> nodeNeighbors(const Node *node);
+    Node *jump(int x, int y, int dX, int dY, Node *start, Node *goal);
+    Node *nextNode(const std::list<Node*> &nodeList);
+    Node *getPoolNode(int x, int y);
+    void initSides();
 
 private:
     const TileMap *m_tileMap;
+    std::vector<Node> m_nodes;
+    std::vector<Point> sides;
 };
 
 #endif // PATHFINDING_H_
