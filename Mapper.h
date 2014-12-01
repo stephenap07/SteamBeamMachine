6b0e8761781804e93e6ae76251917affdef33bf9
Stephen Pridham <stephenap07@gmail.com>
2014-12-01 01:07:27 -0500

    Add George's map generator

---
+++ Mapper.h
@@ -0,0 +1,23 @@
+#ifndef MAPPER_H
+#define MAPPER_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+
+
+
+class Map
+{
+public:
+	int * Init();
+};
+
+
+
+
+
+
+#endif
--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -23,6 +23,44 @@ static T clamp(T value, T min, T max)
 std::vector<Node> Pathfinder::getPath(Node start, Node goal)
 {
     std::vector<Node> result;
+    result.push_back(start);
+
+    std::list<Node*> open_set;
+    std::list<Node*> closed_set;
+
+    open_set.push_back(&start);
+    while (!open_set.empty()) {
+        Node *current = nextNode(open_set);
+        open_set.remove(current);
+        if (*current == goal) {
+            break;
+        }
+        for (Node *neighbor : getNeighbors(current)) {
+            NodePtrIter find_closed = std::find(closed_set.begin(), closed_set.end(), neighbor);
+            if (find_closed != closed_set.end()) {
+                continue; 
+            }
+            float tentative_g = current->g + distance(*current, *neighbor);
+
+            NodePtrIter find_open = std::find(open_set.begin(), open_set.end(), neighbor);
+            if (find_open == open_set.end()) {
+                neighbor->parent = current;
+                neighbor->g = tentative_g;
+                open_set.push_back(neighbor);
+            } else if ((*find_open)->f() > (tentative_g + neighbor->h)) {
+                neighbor->parent = current;
+                neighbor->g = tentative_g;
+                open_set.push_back(neighbor);
+            }
+        }
+        closed_set.push_back(current); 
+    }
+
+    Node *p = goal.parent;
+    while (p) {
+        result.push_back(*p);
+        p = p->parent;
+    }
 
     return result;
 }
@@ -99,3 +137,26 @@ Node Pathfinder::jump(int cX, int cY, int dX, int dY, const Node &start, const N
     return jump(nextX, nextY, dX, dY, start, goal);
 }
 
+Node *Pathfinder::nextNode(std::list<Node*> &nodeList)
+{
+    if (nodeList.empty())
+        return nullptr;
+
+    Node *lowest = nullptr;
+    for (auto node : nodeList) {
+        if (lowest == nullptr) {
+            lowest = node;
+        } else if (node->f() < lowest->f()) {
+            lowest = node;
+        }
+    }
+
+    return lowest;
+}
+
+std::vector<Node*> Pathfinder::getNeighbors(const Node *node)
+{
+    std::vector<Node*> neighbors;
+
+    return neighbors;
+}
